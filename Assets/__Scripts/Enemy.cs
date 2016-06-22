ebc1351059acb926eebe14cad133fee56ca11345
miketranh <miketranh21@gmail.com>
2016-06-22 13:25:44 -0400

    Change1
    Enemy added
    Currently only enemy1 works

---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public float speed=10f;
+	public float fireRate=0.3f;
+	public float health=10;
+	public int score=100;
+	public bool _____________;
+	public Bounds bounds;
+	public Vector3  boundsCenterOffset;
+
+	void Awake() {
+		InvokeRepeating ("CheckOffscreen", 0f, 2f);
+	}
+
+	void Update() {
+		Move();
+	}
+	public virtual void Move(){
+		Vector3 tempPos = pos;
+		tempPos.y -= speed * Time.deltaTime;
+		pos = tempPos;
+	}
+	public Vector3 pos {
+		get {
+			return(this.transform.position);
+		}
+		set {
+			this.transform.position = value;
+		}
+	}
+	void CheckOffscreen() {
+		if (bounds.size == Vector3.zero) {
+			bounds = Utils.CombineBoundsOfChildren (this.gameObject);
+			boundsCenterOffset = bounds.center - transform.position;
+		}
+		bounds.center = transform.position + boundsCenterOffset;
+		Vector3 off = Utils.ScreenBoundsCheck (bounds, BoundsTest.offScreen);
+		if ( off != Vector3.zero){
+			if (off.y < 0) {
+				Destroy( this.gameObject);
+			}
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+}
